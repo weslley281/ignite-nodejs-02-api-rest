07943c5cb48a900be1ba99f3d651c5a0860c7527
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-03-26 07:54:16 -0400

    a

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import fastify from 'fastify';
+import { knex } from './database';
 
 const app = fastify();
 
-app.get('/hello', (req, res) => {
-  return 'Olá mundo';
+app.get('/', async (req, res) => {
+  const tables = await knex('sqlite_schema').select('*');
+  return tables;
 });
 
 const port = 5000;
 app.listen({ port: port }).then(() => {
-  console.log(`The server is running on ${port}`);
+  console.log(`The server is running on http://localhost:${port}`);
 });
