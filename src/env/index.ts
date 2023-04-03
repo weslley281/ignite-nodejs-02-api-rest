ff4b35a4f2ac0bb21f51435694beca5016870218
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-04-03 06:40:22 -0400

    a

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,17 +1,24 @@
-import 'dotenv/config';
+import { config } from 'dotenv';
 import { z } from 'zod';
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' });
+} else {
+  config();
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(5000),
+  PORT: z.number().default(3333),
 });
 
 const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
-  console.error('Variavel Global Invalida', _env.error.format());
-  throw new Error('ariavel Global Invalida');
+  console.error('⚠️ Invalid environment variables', _env.error.format());
+
+  throw new Error('Invalid environment variables.');
 }
 
 export const env = _env.data;
--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,5 +1,6 @@
-import { afterAll, beforeAll, describe, expect, it, test } from 'vitest';
-import supertest = require('supertest');
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest';
+import { execSync } from 'node:child_process';
+import supertest from 'supertest';
 import { app } from '../src/app';
 
 describe('Transactions routes', () => {
@@ -11,10 +12,19 @@ describe('Transactions routes', () => {
     await app.close();
   });
 
-  it('User can create a new transaction', async () => {
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all');
+    execSync('npm run knex migrate:latest');
+  });
+
+  it('should be able to create a new transaction', async () => {
     await supertest(app.server)
       .post('/transactions')
-      .send({ title: 'New Transaction', amount: 5000, type: 'credit' })
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
       .expect(201);
   });
 
